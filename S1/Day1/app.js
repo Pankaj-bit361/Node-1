182d907e262343dddfa4b750ff00ead9c07dc2a5
Pankaj-bit361 <pnkjvshst1@gmail.com>
2023-04-05 21:49:05 +0530

    done

---
+++ S1/Day1/app.js
@@ -0,0 +1,7 @@
+
+const {add,sub,mult,div}=require("./index")
+
+add(10,10)
+sub(10,10)
+mult(10,10)
+div(10,10)
---
+++ S1/Day1/index.js
@@ -0,0 +1,17 @@
+
+ const add=(a,b)=>{
+console.log(a+b)
+}
+
+const sub=(a,b)=>{
+    console.log(a-b)
+    }
+
+    const mult=(a,b)=>{
+        console.log(a*b)
+        }
+
+        const div=(a,b)=>{
+            console.log(a/b)
+            }
+module.exports ={add,sub,mult,div}
